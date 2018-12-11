1d516b5453a9a7d316c5672ce97bda6888566dbe
armadi21 <45292106+armadi21@users.noreply.github.com>
2018-12-11 20:37:47 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+///Jangan hapus tanpa kutip nya(")
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+///Ganti xxxxxxx dengan punya kalian
+///Capture dulu apk nya ambil yang di butuhkan
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh3eWr7qRarWle86wo36wiYanzYSmcpi1hYlmhXqn3It3oZeu0Hqis5OCla2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "ffb51fe73a7bb982";
+$os="android";
+$sign="e4e56ee5a5a018878984b67b05f2df027b210e485b7ad71f5e8d0cf52d59c620";
---
+++ config2.php
@@ -0,0 +1,9 @@
+<?php
+///Jangan hapus tanpa kutip nya(")
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+///Ganti xxxxxxx dengan punya kalian
+///Capture dulu apk nya ambil yang di butuhkan
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh3zNur1-oMC1i8yxfY-giWSrzY-2gWTAm4lmkKC_mIt6g5euqqOytNt3zK2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "94248920714e9da8";
+$os="android";
+$sign="6d5797f6a5525995fe7a505df546d5032c15dfc8323f9dba59e45403877fa50d";
